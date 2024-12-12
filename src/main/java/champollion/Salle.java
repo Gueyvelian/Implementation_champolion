da1a8dc87c80085dbc959369936b899975c49cbf
lgueyv00 <gueyvelian.lucie@gmail.com>
2024-12-12 10:15:55 +0100

    deuxieme commit avec la classe Salle

---
+++ src/main/java/champollion/Salle.java
@@ -0,0 +1,11 @@
+package champollion;
+
+public class Salle {
+    private String intiule;
+    public int capacite;
+
+    public Salle(String intiule, int capacite) {
+        this.intiule = intiule;
+        this.capacite = capacite;
+    }
+}
